5a6196e8ed78812c26d1e3503ab5817bfc15747f
Muhammad ALi <161317819+WazirAly07@users.noreply.github.com>
2024-05-04 17:59:56 +0000

    loops

--- basic/index.ts
+++ basic/index.ts
@@ -6,10 +6,18 @@ console.log("----------Well come to Loops--------------")
 
 //To print staric *
 
-for(let i = 0; i < 5 ; ++i){
-    for(let j = 0; j < i ; ++j ){
-        console.log("*");
-    }
-    console.log("\n");
+// for(let i = 0; i < 5 ; ++i){
+//     for(let j = 0; j < i ; ++j ){
+//         console.log("*");
+//     }
+//     console.log("\n");
     
+// }
+
+//Factorial of 2 (you used any number)
+
+for(let i =0; i<10 ;++i){
+    let num = i * 2 ;
+    console.log(num);
+
 }
