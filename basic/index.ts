59179c87f38f6af3260fe02634aa0825aa9e9e52
Muhammad ALi <161317819+WazirAly07@users.noreply.github.com>
2024-05-04 17:41:27 +0000

    Add for loops only

---
+++ basic/index.ts
@@ -0,0 +1,15 @@
+console.log("----------Well come to Loops--------------")
+
+// for(let i=0;i<4;++i){
+//     console.log("Ali is writting a code.........");
+// }
+
+//To print staric *
+
+for(let i = 0; i < 5 ; ++i){
+    for(let j = 0; j < i ; ++j ){
+        console.log("*");
+    }
+    console.log("\n");
+    
+}
