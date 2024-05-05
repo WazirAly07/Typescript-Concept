04cfbe9332090e46b7419425c3337cd54c1886ab
Muhammad ALi <161317819+WazirAly07@users.noreply.github.com>
2024-05-05 12:48:36 +0000

    no too much

--- basic/while_loop.ts
+++ basic/while_loop.ts
@@ -1,6 +1,7 @@
 console.log("--------While loops---------------");
-let i=0;
-while(i < 10){
-    console.log(`value of i is: ${i}`);
-    ++i;
-}
+
+// let i=0;
+// while(i < 10){
+//     console.log(`value of i is: ${i}`);
+//     ++i;
+// }
