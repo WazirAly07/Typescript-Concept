d9771e13bf9e34d00d38895f74f1ac44d5276445
Muhammad ALi <161317819+WazirAly07@users.noreply.github.com>
2024-05-22 18:30:24 +0000

    NO thing

--- basic/ifelse.ts
+++ basic/ifelse.ts
@@ -1,18 +1,20 @@
+import prompt from "inquirer/lib/ui/prompt";
 
 console.log("--------If Else Statement------------");
 
-let number = 40;
-if(number>=80){
-    console.log(`Nice you got A+ grade and your obtain number is [${number}]`);
-}
-else if (number>=70 && number<80){
-    console.log(`You got B grade and your obtain number is [${number}]`);
-}
-else if (number>=60 && number<70){
-    console.log(`Need Har work you got C grade and your obtain number is [${number}]`);
-}
-else if (number<60){
-    console.log(`Try again you Fail and your obtain number is [${number}]`);
-}
+// let number = 40;
+// if(number>=80){
+//     console.log(`Nice you got A+ grade and your obtain number is [${number}]`);
+// }
+// else if (number>=70 && number<80){
+//     console.log(`You got B grade and your obtain number is [${number}]`);
+// }
+// else if (number>=60 && number<70){
+//     console.log(`Need Har work you got C grade and your obtain number is [${number}]`);
+// }
+// else if (number<60){
+//     console.log(`Try again you Fail and your obtain number is [${number}]`);
+// }
 
 
+let vovel = prompt("Enter a words");
